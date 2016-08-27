ba20e03728084d18cbc1b07d02e3131cf24db6dd
integer <kiryxa00@gmail.com>
2016-08-28 00:30:51 +0600

    purged jquery, added js, img size fix, cleanup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,70 @@
+var feature = document.querySelector("#js-feature-offer");
+var feature_heading = document.getElementById("js-feature-heading");
+
+var slider = document.querySelector("#js-slider");
+var slider_button_1 = document.querySelector("#js-button-1");
+var slider_button_2 = document.querySelector("#js-button-2");
+var slider_button_3 = document.querySelector("#js-button-3");
+
+var feedback = document.querySelector("#js-feedback");
+var modal = document.querySelector("#js-modal");
+var modal_close = document.querySelector("#js-modal__close")
+var overlay = document.querySelector("#js-overlay");
+
+
+
+slider.style.visibility="visible";
+feedback.style.visibility="visible";
+
+slider_button_1.addEventListener("click", function(event){
+  document.body.classList.add("slider1");
+  document.body.classList.remove("slider2");
+  document.body.classList.remove("slider3");
+  feature.style.background = "url(img/morojenko.png) no-repeat top";
+  feature_heading.innerHTML = "Крем-брюле и пломбир с малиновым джемом";
+
+});
+slider_button_2.addEventListener("click", function(event){
+  document.body.classList.add("slider2");
+  document.body.classList.remove("slider1");
+  document.body.classList.remove("slider3");
+  feature.style.background = "url(img/morojenko-2.png) no-repeat top";
+  feature_heading.innerHTML = "Шоколадный пломбир и лимонный сорбет";
+
+  
+});
+slider_button_3.addEventListener("click", function(event){
+  document.body.classList.add("slider3");
+  document.body.classList.remove("slider1");
+  document.body.classList.remove("slider2");
+  feature.style.background = "url(img/morojenko-3.png) no-repeat top";
+  feature_heading.innerHTML = "Пломбир с помадкой и клубничный щербет";
+  
+});
+
+feedback.addEventListener("click", function(event){
+  event.preventDefault();
+  overlay.classList.add("modal-overlay--show");
+  modal.classList.add("modal-feedback--show");
+
+});
+
+modal_close.addEventListener("click", function() {
+  event.preventDefault();
+  overlay.classList.remove("modal-overlay--show");
+  modal.classList.remove("modal-feedback--show");
+
+});
+
+    window.addEventListener("keydown", function(){
+      if (event.keyCode === 27) {
+        if (overlay.classList.contains("modal-overlay--show")){
+              modal.classList.remove("modal-feedback--show");
+              overlay.classList.remove("modal-overlay--show");
+            }
+          }
+        });
+
 ymaps.ready(function () {
     var myMap = new ymaps.Map('map', {
                center: [59.938429885190054, 30.32991749740603],
@@ -20,79 +87,3 @@ myPlacemark = new ymaps.Placemark([59.93866675783276, 30.32307250000002], {
 
     myMap.geoObjects.add(myPlacemark);
 });
-
-
-
-
-
-$(document).ready(function() {
-   $(".page-feature__buttons-carousel").css("visibility","visible");
-   $(".text-feedback__button").css("visibility","visible");
-
-   	$(".carousel__btn-1").click(function() {
-   		
- 		$("body").removeClass('slider2 slider3 slider2-offer slider3-offer');
- 		$("body").addClass('slider1');
- 	 	$(".page-feature-offer").removeClass('slider2-offer slider3-offer');
- 		$(".page-feature-offer").addClass('slider1-offer');
- 		$(".page-feature__heading").text("Крем-брюле и пломбир с малиновым джемом");
-
- 		});
-
-
-
-
-
-
- 		$(".carousel__btn-2").click(function() {
-   		
- 		$("body").removeClass('slider1 slider3 slider1-offer slider3-offer');
- 		$("body").addClass('slider2');
-		$(".page-feature-offer").removeClass('slider1-offer slider3-offer');
- 		$(".page-feature-offer").addClass('slider2-offer');
- 		$(".page-feature__heading").text("Шоколадный пломбир и лимонный сорбет ");
-
-
- 		});
-
- 		$(".carousel__btn-3").click(function() {
-   		
- 		$("body").removeClass('slider1 slider2');
- 		$("body").addClass('slider3');
- 		$(".page-feature-offer").removeClass('slider1-offer slider2-offer');
- 		$(".page-feature-offer").addClass('slider3-offer')
- 		$(".page-feature__heading").text("Пломбир с помадкой и клубничный щербет");
- 		});
-
- 		$(".text-feedback__button").click(function() {
-
-
- 			$(".modal-overlay").addClass('modal-overlay--show');
- 			$(".modal-feedback").addClass('modal-feedback--show');
-
-
- 		});
-
- 		$(".modal__close").click(function() {
- 			$(".modal-feedback").removeClass('modal-feedback--show');
- 			$(".modal-overlay").removeClass('modal-overlay--show');
-
-
- 		});
-		
-
-
-      
- 		$(window).keydown(function() {
-
- 			  if (event.keyCode === 27)  {
- 			  	if ($(".modal-feedback").hasClass('modal-feedback--show')) {
- 		 			  $(".modal-feedback").removeClass('modal-feedback--show');
- 			 			 $(".modal-overlay").removeClass('modal-overlay--show');
-					}
- 			  }
- 			});
-
-
-
-	});
